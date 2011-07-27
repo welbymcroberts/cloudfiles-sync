ef2536633f032bb33283fc16abe4e5cb92cf137b
Welby McRoberts <welby@whmcr.com>
2011-07-27 08:28:39 +0100

    Inital work on CLI options / config file

--- cloud-sync.py
+++ cloud-sync.py
@@ -1,3 +1,5 @@
+import ConfigParser
+import optparse
 import sys
 from log import Logging
 import threading
@@ -6,22 +8,46 @@ from cloud_providers.swift import *
 from file_lists.local import *
 from file_lists.swift import *
 
-def setup_logging():
+def setup_logging(console_level="WARNING",file_level="WARNING",file_name="cloud-sync.log"):
     _logger = Logging()
-    _logger.setup(console_level="DEBUG")
+    _logger.setup(console_level=console_level,file_level=file_level,file_name=file_name)
 
 def setup_config():
-    pass
+    op = optparse.OptionParser()
+    cp = ConfigParser.ConfigParser()
+    cp.read(['/etc/cloud-sync/cloudsync.ini', os.path.expanduser('~/.cloudsync.ini') ])
+    general = op.add_option_group('General')
+    general.add_option('-d','--console-level', dest='console_level', help='Log Level for Console (DEBUG,INFO,WARNING,ERROR,CRITICAL)', default=cp.get('general','console_level'))
+    general.add_option('-l','--file-level', dest='file_level', help='Log Level for File (DEBUG,INFO,WARNING,ERROR,CRITICAL)',default=cp.get('general','file_level'))
+    general.add_option('-f','--logfile', dest='log_file', help='Log File Name',default=cp.get('general','log_file'))
+    general.add_option('-m','--md5', dest="md5", help="Enable MD5 Comparison",default=cp.get('general','md5'))
+    api = op.add_option_group('API')
+    api.add_option('-u','--username', dest="username", help="API Username (ex: 'welby.mcroberts')",default=cp.get('api','username'))
+    api.add_option('-k','--key', dest="key", help="API Key (ex 'abcdefghijklmnopqrstuvwxyz12345",default=cp.get('api','key'))
+    api.add_option('-a','--authurl', dest="authurl", help='Auth URL (ex: https://lon.auth.api.rackspacecloud.com/v1.0 )',default=cp.get('api','uri'))
+    api.add_option('-s','--connections',dest="connections",help='Number of Connections to API',default=cp.get('api','connections'))
+    api.add_option('-t','--timeout',dest="timeout",help='API Timeout',default=cp.get('api','timeout'))
+    api.add_option('-n','--servicenet',dest="servicenet",help='Use Servicenet',default=cp.get('api','servicenet'))
+    api.add_option('-z','--useragent',dest="useragent",help='Override Useragent',default=cp.get('api','useragent'))
 
-setup_config()
-setup_logging()
+    cloud = op.add_option_group('Cloud')
 
-##############################################
-##############################################
-##############################################
-##############################################
-swift = Swift(username='TEST',api_key='TEST')
-swift.connect(pool_count=2)
+
+    return op.parse_args()
+
+
+(op_results,op_args) = setup_config()
+
+
+setup_logging(op_results.console_level, op_results.file_level, op_results.log_file)
+
+
+swift = Swift(username=op_results.username,api_key=op_results.key,
+              timeout=int(op_results.timeout),
+              servicenet=op_results.servicenet,
+              useragent=op_results.useragent,
+              auth_url=op_results.authurl)
+swift.connect(pool_count=int(op_results.connections))
 container='bobette'
 
 
--- readme.md
+++ readme.md
@@ -1,25 +1,34 @@
-# Cloudfiles-sync
+# Cloud-sync
 
-This is a script that allows the syncronisation of Rackspace Cloudfiles (Or any SWIFT object store) with a local directory. Currently it allows the user to pipe a list of files into it that are to be uploaded to cloud files.
+This is a script that allows the syncronisation of Rackspace Cloudfiles (Or any SWIFT object store)
+with a local directory. Currently it allows the user to pipe a list of files into it that are to be
+uploaded to cloud files.
+
 
 Features
 ========
-* Lightweight - Currently the only non standard python library required is python-cloudfiles. The aim is NOT to require anything else, and hence ensuring maximum portability
+* Lightweight - Currently the only non standard python library required is python-cloudfiles.
+    The aim is NOT to require anything else, and hence ensuring maximum portability
 * Support for Cloudfiles UK and US along with any SWIFT object store
 * Comparision of files to ensure Bandwidth is not wasted by comparing the remote & local MD5 and Modified Time
-* Removal of files that do not exist in the source from the destination
-* Quiet by default - Allows the script to run as a cron without any noise
-* Verbose mode - Useful for logging or debuging
-* Progress - a simple progress report
 
 See the projects github wiki page for more info
 https://github.com/welbymcroberts/cloudfiles-sync/wiki
 
 Example usage
 =============
-`cd /backups && find -type f | python cfsync.py -v -s -m`
 
 Authors
 ===============
 * Welby McRoberts <cloudfilessync+welby@whmcr.com> Main developer.
 * Darren Birket - Contributor, Tester and provider of suggestions.
+
+Change Log
+============
+
+1.9.1
+* A rewrite of cloudfiles-sync to support multiple cloud providers, and code cleanup
+* Inital thoughts of threading
+
+1.0.0
+* First version
